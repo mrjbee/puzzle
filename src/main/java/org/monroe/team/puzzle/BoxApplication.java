323c1dadc5bc629514139a9bcb8e3f58f14068c0
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2016-09-18 03:12:05 +0300

    [FIX] Fix compilation issue

--- src/main/java/org/monroe/team/puzzle/BoxApplication.java
+++ src/main/java/org/monroe/team/puzzle/BoxApplication.java
@@ -7,9 +7,7 @@ import net.engio.mbassy.bus.config.Feature;
 import net.engio.mbassy.listener.MetadataReader;
 import net.engio.mbassy.subscription.SubscriptionFactory;
 import net.engio.mbassy.subscription.SubscriptionManagerProvider;
-import org.monroe.team.puzzle.core.bus.MyMetadaReader;
 import org.monroe.team.puzzle.core.events.Event;
-import org.monroe.team.puzzle.core.events.EventBus;
 import org.monroe.team.puzzle.core.events.EventSubscriber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,23 +16,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BoxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoxApplication.class, args)
+    public static void main(String[] args) {
+        SpringApplication.run(BoxApplication.class, args)
                 .registerShutdownHook();
-	}
+    }
 
     //TODO graceful shutdown
-	@Bean(destroyMethod = "shutdown")
-	public MBassador<Event> bus(EventSubscriber... eventSubscribers){
+    @Bean(destroyMethod = "shutdown")
+    public MBassador<Event> bus(EventSubscriber... eventSubscribers) {
         MBassador<Event> mBassador = new MBassador<Event>(
                 new BusConfiguration()
-                .addFeature( new Feature.SyncPubSub()
-                        .setMetadataReader(new MetadataReader())
-                        .setPublicationFactory(new MessagePublication.Factory())
-                        .setSubscriptionFactory(new SubscriptionFactory())
-                        .setSubscriptionManagerProvider(new SubscriptionManagerProvider()))
-                .addFeature(Feature.AsynchronousHandlerInvocation.Default())
-                .addFeature(Feature.AsynchronousMessageDispatch.Default()));
+                        .addFeature(new Feature.SyncPubSub()
+                                .setMetadataReader(new MetadataReader())
+                                .setPublicationFactory(new MessagePublication.Factory())
+                                .setSubscriptionFactory(new SubscriptionFactory())
+                                .setSubscriptionManagerProvider(new SubscriptionManagerProvider()))
+                        .addFeature(Feature.AsynchronousHandlerInvocation.Default())
+                        .addFeature(Feature.AsynchronousMessageDispatch.Default()));
         for (EventSubscriber eventSubscriber : eventSubscribers) {
             mBassador.subscribe(eventSubscriber);
         }
