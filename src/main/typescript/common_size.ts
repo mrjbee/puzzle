322d1d6f920e63fd167c19d816ed91f4d9a8d36b
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2016-10-05 02:45:08 +0300

    [DEBS]Intorduce UI Builders

--- src/main/typescript/common_size.ts
+++ src/main/typescript/common_size.ts
@@ -6,19 +6,11 @@
 class ThumbnailMath {
     
     static MAX_THUMBNAIL_CELL_SIZE = 300
-
-    private _pageWidth:number
+    static DEFAULT = new ThumbnailMath()
+    
+    private _pageWidth:number=-1
     private _cellWidth:number
     private _cellHeight:number
-
-    constructor(width:number) {
-        this._pageWidth = width
-        this._cellWidth = Math.floor(width / 2)
-        if (this._cellWidth > ThumbnailMath.MAX_THUMBNAIL_CELL_SIZE) {
-            this._cellWidth = ThumbnailMath.MAX_THUMBNAIL_CELL_SIZE
-        }
-        this._cellHeight = this._cellWidth * 0.8    
-    }
 
     get pageWidth():number{
         return this._pageWidth
@@ -33,6 +25,14 @@ class ThumbnailMath {
     }
 
     updatePageWidth(width:number){
+        if (this._pageWidth == -1){
+            this._pageWidth = width
+            this._cellWidth = Math.floor(width / 2)
+            if (this._cellWidth > ThumbnailMath.MAX_THUMBNAIL_CELL_SIZE) {
+                this._cellWidth = ThumbnailMath.MAX_THUMBNAIL_CELL_SIZE
+            }
+            this._cellHeight = this._cellWidth * 0.8    
+        }
         this._pageWidth = width
     }
 
