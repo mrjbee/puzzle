4e27b9f6695160c0d3c666469f4764b0d22f8071
Dmytro Khudin <MisterJBee@gmail.com>
2016-11-28 14:23:36 +0200

    [UX] Update rsync and enhance date reporting

--- backup.example.sh
+++ backup.example.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "Start backup at: $(date)"
+
 DEVICE=/dev/sdd
 PARTITION_DEVICE=/dev/sdd1
 LAST_RUN_FILE=/mnt/Storage/last_backup_date
@@ -11,18 +14,19 @@ then
    LAST_BACK_UP_DATE=`cat $LAST_RUN_FILE`
    if [ $LAST_BACK_UP_DATE = $TODAY ];
    then
-	echo "One backup was done today already. Today is $TODAY"
-	udisks --unmount $PARTITION_DEVICE
+        echo "One backup was done today already. Today is $TODAY"
+        udisks --unmount $PARTITION_DEVICE
         udisks --detach $DEVICE
-	exit 1
+        echo "End backup [done nothing] at: $(date)"
+        exit 1
    fi
 fi
 
 echo "Going to mount and perform backup ... "
 udisks --mount $PARTITION_DEVICE
-rsync --partial -r --progress --bwlimit=10000 /mnt/Storage/Catalog/ /media/MediaBackup1/Catalog
+rsync --partial -r -vv --ignore-existing --bwlimit=10000 /mnt/Storage/Catalog/ /media/MediaBackup1/Cata$
 echo $TODAY > $LAST_RUN_FILE
 echo "Backup done. Backup date $TODAY"
 udisks --unmount $PARTITION_DEVICE
 udisks --detach $DEVICE
-echo "Exit from backup procedure"
+echo "End backup at: $(date)"
