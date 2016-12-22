fb737d4393012eb2db930a4f642259dc09e1705c
Dmytro Khudin <MisterJBee@gmail.com>
2016-12-23 01:48:50 +0200

    [SECURITY] Add admin role

--- src/main/java/org/monroe/team/puzzle/security/WebSecurityConfig.java
+++ src/main/java/org/monroe/team/puzzle/security/WebSecurityConfig.java
@@ -3,10 +3,14 @@ package org.monroe.team.puzzle.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntryPoint;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -20,14 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //TODO http://stackoverflow.com/questions/21128058/invalid-csrf-token-null-was-found-on-the-request-parameter-csrf-or-header
                 .csrf().disable()
                 .authorizeRequests()
-            .       antMatchers("/")
-                        .permitAll()
-                            .anyRequest()
-                                .hasRole("USER")
-
+                    .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                    .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                    .anyRequest().hasAnyRole("ADMIN","USER")
             .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new Http401AuthenticationEntryPoint("Basic realm=\"Please provide your media browser credentials\""));
+                    .authenticationEntryPoint(new Http401AuthenticationEntryPoint("Basic realm=\"Please provide your media browser credentials\""));
     }
 
     @Autowired
@@ -36,6 +38,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .inMemoryAuthentication()
                 .withUser("me")
                     .password(System.getProperty("default.password","trust me"))
-                    .roles("USER");
+                    .roles("USER")
+            .and()
+                .withUser("admin")
+                    .password(System.getProperty("default.admin.password","I am admin"))
+                    .roles("ADMIN");
+
     }
 }
